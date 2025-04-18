a7383e0a9a3a5c721ea4af52a36e6cb3fd4477da
leonardo-bravop <leo.bravoparedes@gmail.com>
2025-04-18 13:43:44 -0500

    Create mobile design

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import './style.scss';
+import mobileSrc from './assets/images/image-product-mobile.jpg'
+import cart from './assets/images/shopping-cart.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <main>
+   <div class="product-card">
+    <picture class="image">
+      <img src="${mobileSrc}">
+    </picture>
+    <div class="description">
+      <span class="type">Perfume</span>
+      <h1>Gabrielle Essence Eau De Parfum</h1>
+      <p>
+        A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+        Perfumer-Creator for the House of CHANEL.
+      </p>
+      <div class="price">
+        <span class="main-price">$149.99</span>
+        <span class="original-price">$169.99</span>
+      </div>
+      <button class="cta">
+        <img src="${cart}" class="icon"/>
+        <span>Add to Cart</span>
+      </button>
+    </div>
+   </div>
+  </main>
+`
