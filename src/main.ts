d4a4f886c0dbfd90d590611ed16972a584868595
leonardo-bravop <leo.bravoparedes@gmail.com>
2025-04-27 17:28:27 -0500

    Improve product images alt descriptions

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,30 @@ import mobileSrc from './assets/images/image-product-mobile.jpg'
 import desktopSrc from './assets/images/image-product-desktop.jpg'
 import cart from './assets/images/shopping-cart.svg'
 
+const productData = {
+  name: "Gabrielle Essence Eau De Parfum",
+  type: "perfume",
+  description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+  mainPrice: 149.99,
+  originalPrice: 169.99,
+  mobileSrc,
+  desktopSrc
+}
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <main>
    <div class="product-card">
     <picture class="image">
-      <img src="${mobileSrc}" class="mobile-img" alt="product image">
-      <img src="${desktopSrc}" class="desktop-img" alt="product image">
+      <img src="${productData.mobileSrc}" class="mobile-img" alt="${productData.name}">
+      <img src="${productData.desktopSrc}" class="desktop-img" alt="${productData.name}">
     </picture>
     <div class="description">
-      <span class="type">Perfume</span>
-      <h1>Gabrielle Essence Eau De Parfum</h1>
-      <p>
-        A floral, solar and voluptuous interpretation composed by Olivier Polge, 
-        Perfumer-Creator for the House of CHANEL.
-      </p>
+      <span class="type">${productData.type}</span>
+      <h1>${productData.name}</h1>
+      <p>${productData.description}</p>
       <div class="price">
-        <span class="main-price">$149.99</span>
-        <span class="original-price">$169.99</span>
+        <span class="main-price">$${productData.mainPrice}</span>
+        <span class="original-price">$${productData.originalPrice}</span>
       </div>
       <button class="cta">
         <img src="${cart}" class="icon" alt="cart icon"/>
